e32e943b20d5cbe1d7b4510e0f1095bdb550abd9
Michał Szymański <smiszym@gmail.com>
2018-11-06 16:21:34 +0100

    Trivial implementation of rate_action() for MazeF4

--- gym_maze/envs/MazeF4.py
+++ gym_maze/envs/MazeF4.py
@@ -13,3 +13,14 @@ class MazeF4(AbstractMaze):
             [1, 0, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1],
         ]))
+        self.solution = np.matrix([
+            [9, 9, 9, 9, 9, 9, 9],
+            [9, 2, 2, 2, 2, 9, 9],
+            [9, 1, 9, 9, 9, 9, 9],
+            [9, 0, 7, 6, 6, 9, 9],
+            [9, 0, 9, 9, 9, 9, 9],
+            [9, 9, 9, 9, 9, 9, 9],
+        ])
+
+    def rate_action(self, action):
+        return 1.0 if action == self.solution[self.pos_y, self.pos_x] else 0.0
